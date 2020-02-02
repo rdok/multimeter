f83f15875a407296ddde9102c2d4292c173f483b
Rizart Dokollari <r.dokollari@gmail.com>
2020-02-02 13:51:43 +0000

    Backup AWS.S3: Set expected size due to big size.

--- backup.sh
+++ backup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-NAME=$([ -z "$1" ] && date +%F_%H-%M || echo "$1" )
+NAME=$([ -z "$1" ] && date +%F_%H-%M || echo "$1")
 NAME="${NAME}.img.gz"
 
-sudo dd bs=4M if=/dev/sdb status=progress \
-   | gzip \
-   | aws s3 cp --storage-class DEEP_ARCHIVE --profile falcon9 - \
+(
+  sudo dd bs=4M if=/dev/sdb status=progress |
+    gzip |
+    aws s3 cp \
+      --storage-class DEEP_ARCHIVE \
+      --profile falcon9 \
+      --expected-size=$((1024 * 1024 * 1024 * 35)) \
+      - \
       "s3://backups-rdok/falcon9/${NAME}"
+) 2>&1 | tr '\r' '\n'
