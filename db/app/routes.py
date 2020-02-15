3d65c4f78ebe93b5afa4191d0302de9e3e023151
Rizart Dokollari <r.dokollari@gmail.com>
2020-02-15 15:13:52 +0000

    db.service: api end point to store a measurement.

--- db/app/routes.py
+++ db/app/routes.py
@@ -1,11 +1,14 @@
+import datetime
+
 from flask import jsonify, request
 
-from app import application
+from app import application, db
 from app.forms import StoreTemperatureHumidityReadingForm
+from app.models import TemperatureHumidityReading
 
 
 @application.route("/db/")
-def db():
+def default():
     return jsonify({"description": "Expose stored data, such as measurements."})
 
 
@@ -20,9 +23,22 @@ def measurements():
 @application.route("/db/temperature-humidity-readings/", methods=['POST'])
 def store_measurement():
     data = request.json
+
     form = StoreTemperatureHumidityReadingForm().from_json(data)
 
     if not form.validate():
         return jsonify(errors=form.errors), 422
 
-    return jsonify(data)
+    record = TemperatureHumidityReading(
+        sensorID=data['sensorID'],
+        createdAt=datetime.datetime.now(),
+        takenAt=datetime.datetime.strptime(data['takenAt'], "%Y-%m-%d %H:%M:%S"),
+        temperature_c=data['temperatureC'],
+        temperature_f=data['temperatureF'],
+        humidity=data['humidity'],
+    )
+
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify({'id': record.id})
--- db/config.py
+++ db/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data/app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(32)
